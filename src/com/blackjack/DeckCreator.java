aef15b7348c0a11d74096871e92c30542f557f66
Dmitry Filonenko <d.filonenko@kezzler.com>
2018-03-29 11:14:07 +0200

    Add option of shuffle to deck creator

--- src/com/blackjack/DeckCreator.java
+++ src/com/blackjack/DeckCreator.java
@@ -9,11 +9,23 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.stream.Collectors;
 
 public class DeckCreator {
   public static @NotNull Iterator<Card> fromStream(@NotNull InputStream inputStream) throws IOException {
+    return readCardsFromStream(inputStream).iterator();
+  }
+
+  public static @NotNull Iterator<Card> shuffledFromStream(@NotNull InputStream inputStream) throws IOException {
+    ArrayList<Card> cardsList = readCardsFromStream(inputStream);
+    Collections.shuffle(cardsList);
+    return cardsList.iterator();
+  }
+
+  @NotNull
+  private static ArrayList<Card> readCardsFromStream(@NotNull InputStream inputStream) throws IOException {
     ArrayList<Card> result = new ArrayList<>();
     InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
     BufferedReader reader = new BufferedReader(streamReader);
@@ -22,7 +34,6 @@ public class DeckCreator {
                         .map(card -> Card.createCard(card.trim()))
                         .collect(Collectors.toList()));
     }
-
-    return result.iterator();
+    return result;
   }
 }
